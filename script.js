251aebc2ba01b16b368f9f64b58dbe8e27c3df2d
HarshitTyagi651 <154967440+HarshitTyagi651@users.noreply.github.com>
2024-08-13 14:49:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function shortcut(s1, s2) {
   // your code here
-}
-
+	let newString = '' 
+	if(s1 && s2){
+		newString = s1[0]+s2[0]
+	}
+	return newString
+} 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
